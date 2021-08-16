e699993c7df175418584b0e0505a10878d15baf1
Poturajko <poturajko@gmail.com>
2021-08-16 18:02:50 +0300

    add admin panel

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ error_reporting(E_WARNING);
 error_reporting(E_ERROR);
 
 use App\Controllers\Admin\CategoryController;
+use App\Controllers\Admin\OrderController;
+use App\Controllers\Admin\ProductController;
 use App\Controllers\AuthController;
 use App\Controllers\CartController;
 use App\Controllers\MainController;
@@ -25,14 +27,27 @@ $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
 $app->router->get('/logout', [AuthController::class, 'logout']);
 
-$app->router->get('/admin', [CategoryController::class, 'index']);
-$app->router->post('/admin/category', [CategoryController::class, 'store']);
-$app->router->get('/admin/category/create', [CategoryController::class, 'create']);
-$app->router->get('/admin/category/{id}', [CategoryController::class, 'show']);
+$app->router->get('/admin/categories', [CategoryController::class, 'index']);
+$app->router->post('/admin/categories', [CategoryController::class, 'store']);
+$app->router->get('/admin/categories/create', [CategoryController::class, 'create']);
+$app->router->get('/admin/categories/{id}', [CategoryController::class, 'show']);
+$app->router->get('/admin/{id}/categories', [CategoryController::class, 'edit']);
+$app->router->post('/admin/categories/{id}', [CategoryController::class, 'update']);
+$app->router->post('/admin/categories/delete/{id}', [CategoryController::class, 'destroy']);
 
+$app->router->get('/admin/products', [ProductController::class, 'index']);
+$app->router->post('/admin/products', [ProductController::class, 'store']);
+$app->router->get('/admin/products/create', [ProductController::class, 'create']);
+$app->router->get('/admin/products/{id}', [ProductController::class, 'show']);
+$app->router->get('/admin/{id}/products', [ProductController::class, 'edit']);
+$app->router->post('/admin/products/{id}', [ProductController::class, 'update']);
+$app->router->post('/admin/products/delete/{id}', [ProductController::class, 'destroy']);
+
+$app->router->get('/admin/orders', [OrderController::class, 'index']);
+$app->router->get('/admin/orders/{order}', [OrderController::class, 'show']);
 
 $app->router->get('/', [MainController::class, 'index']);
-$app->router->get('/categories', [MainController::class, 'categories']);
+$app->router->get('/categories', [MainController::class, 'show']);
 
 $app->router->get('/cart/add/{id}', [CartController::class, 'cartAdd']);
 $app->router->get('/cart/remove/{id}', [CartController::class, 'cartRemove']);
